0509e032a5fab9a5126a9f943819f501a360009d
Nico <Nico.marra@swissonline.ch>
2023-12-04 08:45:17 +0100

    fix: remove Debug mode

--- main.py
+++ main.py
@@ -355,5 +355,5 @@ def liste_dateien_und_ordner(pfad):
 
 
 if __name__ == "__main__":
-    application.run(host="0.0.0.0", debug=True, port=5000)
+    application.run(host="0.0.0.0", debug=False, port=5000)
  
